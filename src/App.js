925d37a56ca2852d4d4ff2f17754c5e0bf48d460
Altynai_UG <altynai.dzhusupovaa@gmail.com>
2023-09-21 21:17:24 +0600

    App js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter } from "react-router-dom";
+import Routing from "./Routing";
+
+// import AuthContextProvider from "./contexts/authContext";
+// import ProductsContextProvider from "./contexts/productContext";
+
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import FavoriteContext from "./contexts/favoriteContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <ProductsContextProvider>
+        <ToastContainer />
+        <AuthContextProvider>
+        <FavoriteContext> */}
+      <BrowserRouter>
+        <Navbar />
+        <Routing />
+      </BrowserRouter>
+      {/* </FavoriteContext>
+        </AuthContextProvider>
+      </ProductsContextProvider> */}
+    </>
   );
 }
 
