43aa99f6e587c4991dec2186537ff50c94c27a4a
Altynai_UG <altynai.dzhusupovaa@gmail.com>
2023-09-25 21:04:32 +0600

    version 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter } from "react-router-dom";
 import Routing from "./Routing";
+
+import AuthContextProvider from "./contexts/authContext";
+import ProductsContextProvider from "./contexts/productContext";
+
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import FavoriteContext from "./contexts/favoriteContext";
 
 function App() {
   return (
     <>
-      <Routing/>
+      <ProductsContextProvider>
+        {/* <ToastContainer /> */}
+        <AuthContextProvider>
+          {/* <FavoriteContext> */}
+
+          <Navbar />
+          <Routing />
+
+          {/* </FavoriteContext> */}
+        </AuthContextProvider>
+      </ProductsContextProvider>
     </>
   );
 }
